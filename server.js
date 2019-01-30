b889df5d79ec478b158953d515ac6078a399fd0f
Jason Wilson <jasonpatrickwilson@gmail.com>
2019-01-30 00:55:04 -0500

    Fixed process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = processs.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
